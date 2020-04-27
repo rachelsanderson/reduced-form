0716d0db6a52bbd9be3038befe1f7e4b1a073442
Rachel Anderson <rachelsa@princeton.edu>
2020-04-27 12:20:44 -0400

    stopping for meeting

--- Analyses/GeneratorData/analyze_gen_2018.R
+++ Analyses/GeneratorData/analyze_gen_2018.R
@@ -60,15 +60,19 @@ calc_num_quarters <- function(dates) {
 
 # Redo the solar pictures -------------------------------------------------
 
-solar <- filter(gen_data_merged, energy_source_1=="SUN" & operating_year >=2006)
 omitList = c("IPP CHP", "Industrial Non-CHP", "Industrial CHP")
-solar_tot <- solar %>% filter(operating_year >2010, !(sector_name %in% omitList)) %>%
-  group_by(operating_quarter, sector_name, grant_program) %>%
+
+solar <- filter(gen_data_merged, technology == "Solar Photovoltaic",
+                  operating_year >=2010, region != "Hawaii",
+                region!= "Alaska",
+                !(sector_name %in% omitList))
+
+solar_tot <- solar %>% group_by(operating_quarter, sector_name, grant_program) %>%
   summarise(tot_cap = sum(nameplate_cap))
 
 # total MW additions by year and sector
 ggplot(solar_tot, aes(x=operating_quarter,y=tot_cap, fill = sector_name, 
-                      color=sector_name)) +
+                      )) +
   geom_bar(stat='identity',position='stack', alpha = 0.8) + 
   scale_x_yearqtr(format = "%YQ%q", 
                   breaks = seq(from = min(solar_tot$operating_quarter)+.75, 
@@ -80,7 +84,7 @@ ggplot(solar_tot, aes(x=operating_quarter,y=tot_cap, fill = sector_name,
   ylab("Total Capacity (MW)\n") + 
   geom_vline(xintercept=as.yearqtr("2016 Q4"), linetype='dotted') +
   theme(axis.text.x = element_text(angle=90),
-        legend.position="bottom",
+        legend.position="right",
         legend.title=element_blank())
 
 ggsave(paste0(figDir,"solar_additions.png"),width=10,height=7)
@@ -98,105 +102,65 @@ ggplot(solar_tot, aes(x=operating_quarter,y=tot_cap, fill = grant_program)) +
   # scale_fill_discrete(label= c("No Loan Grant","1603 Grant")) + 
   geom_vline(xintercept=as.yearqtr("2016 Q4"), linetype='dotted') +
   theme(axis.text.x = element_text(angle=90),
-        legend.position="bottom",
+        legend.position=c(0.2,0.8),
         legend.title=element_blank())
 
 ggsave(paste0(figDir,"solar_loan_grants.png"),width=10,height=7)
 
 
-
-solar_ann <- solar %>% filter(operating_year >2008, !(sector_name %in% omitList)) %>%
-  group_by(operating_year, sector_name) %>%
-  summarise(tot_cap = sum(nameplate_cap))
-
-ggplot(solar_ann, aes(x=operating_year,y=tot_cap, fill = sector_name, color=sector_name)) +
+## annual additions graph
+solar %>% group_by(operating_year, sector_name, grant_program) %>%
+  summarise(tot_cap = sum(nameplate_cap)) %>%
+  ggplot(aes(x=operating_year,y=tot_cap, fill = grant_program)) +
   geom_bar(stat='identity',position='stack', alpha = 0.8) + 
   theme_bw() + 
   scale_x_continuous(breaks=seq(2008,2018,1)) +
   xlab("\nStart Operation Date") +
   ylab("Total Capacity (MW)\n") + 
   theme(axis.text.x = element_text(angle=90),
-        legend.position="bottom",
+        legend.position=c(0.2,0.8),
         legend.title=element_blank())
 ggsave(paste0(figDir, 'sollar_add_ann.png'),width=10,height=7)
 
-ggplot(solar %>% filter(operating_year >=2010), aes(x=operating_date,
-                         fill=I('blue'),
-                         col=I("black"),
-                        )) + 
-  geom_histogram(bins=calc_num_quarters(solar$operating_date)*2,
-                 boundary = 0.5, alpha=0.6) +
-  geom_vline(xintercept = as.Date("2016-11-17"), 
-             linetype='dotted') + 
-  scale_x_date("Operating Date",
-               breaks = date_breaks("6 months"),
-               labels = format_quarters) +
-  theme(axis.text.x = element_text(angle=90))
 
+# Regional Analysis -------------------------------------------------------
 
-ggsave(paste0(figDir,"solar_nAdditions.png"),width=10,height=7)
-# Work in the utility data ------------------------------------------------
- 
-# is there a region variable? 
-gen_data_merged %>% group_by(street_address, technology) %>% 
-  summarise(tot_cap = sum(nameplate_cap),
-            nUnits = n()) %>% 
-  filter(technology == "Solar Photovoltaic") %>% arrange(desc(tot_cap)) %>%
-  ungroup() %>% 
-  mutate(share = tot_cap/sum(tot_cap)) %>% View()
-  
-gen_data_merged %>% filter(technology == "Solar Photovoltaic",
-                           operating_year > 2008) %>%
-  group_by(operating_quarter, street_address) %>% 
-  summarise(tot_cap = sum(nameplate_cap),
-            nUnits = n()) %>% 
-  ungroup() %>% group_by(operating_quarter) %>%
-  mutate(share = tot_cap/sum(tot_cap)) %>%  
-  arrange(operating_quarter, desc(tot_cap)) %>% 
-  filter(share >= 0.05) %>% 
-  ggplot(aes(x=operating_quarter,y=tot_cap, fill=street_address)) +
+solar %>% group_by(operating_quarter, grant_program, region) %>%
+  summarise(tot_cap = sum(nameplate_cap)) %>% 
+  ggplot(aes(x=operating_quarter,y=tot_cap, fill=grant_program)) + 
   geom_bar(stat='identity',position='stack') +
-  theme(legend.position = "none")
-
-
-# Make data for regressions  -----------------------------------------------------
-
-# observation is capacity addition in state by quarter 
-# y_st
-
-solar_raw <- filter(gen_data_merged, energy_source_1=="SUN" & operating_year >=2006)
-omitList = c("IPP CHP", "Industrial Non-CHP", "Industrial CHP")
-solar <- solar_raw %>%
-  filter(!(sector_name %in% omitList),operating_year >= 2010,
-         loan_grant=="False") %>%
-  group_by(operating_quarter, sector_name, region) %>%
-  summarise(tot_cap = sum(nameplate_cap)) 
-  
-ggplot(solar, aes(x=operating_quarter,y=tot_cap, fill=region)) +
-  geom_bar(stat='identity',position='stack') +
-  scale_x_yearqtr(format = "%YQ%q", 
-                  breaks = seq(from = min(solar$operating_quarter)+.75, 
-                               to = max(solar$operating_quarter), by = 1),
-                  limits=c(min(solar$operating_quarter), 
-                           max=max(solar$operating_quarter))) +
   theme_bw() + 
+  facet_wrap(~region, scales="free_y", nrow=2) + 
   xlab("\nStart Operation Date") +
-  ylab("Total Capacity (MW)\n") + 
+  ylab("Capacity (MW)\n") + 
   geom_vline(xintercept=as.yearqtr("2016 Q4"), linetype='dotted') +
   theme(axis.text.x = element_text(angle=90),
         legend.position="bottom",
         legend.title=element_blank())
 
-ggsave(paste0(figDir,"solar_additions_region.png"),width=10,height=7)
+ggsave(paste0(figDir,"solar_additions_region.png"),width=14,height=8)
+
+
+solar %>% group_by(operating_year, grant_program, region) %>%
+  summarise(tot_cap = sum(nameplate_cap)) %>% 
+  ggplot(aes(x=operating_year,y=tot_cap, fill=grant_program)) + 
+  geom_bar(stat='identity',position='stack') +
+  theme_bw() + 
+  facet_wrap(~region, scales="free_y", nrow=2) + 
+  xlab("\nStart Operation Date") +
+  ylab("Capacity (MW)\n") + 
+  geom_vline(xintercept=2016, linetype='dotted') +
+  theme(axis.text.x = element_text(angle=90),
+        legend.position="bottom",
+        legend.title=element_blank())
+ggsave(paste0(figDir,"solar_additions_region_ann.png"),width=14,height=8)
 
 
 # by state ----------------------------------------------------------------
 
-solar_reg <- solar_raw %>%
-  filter(!(sector_name %in% omitList),operating_year >= 2010) %>%
+solar_reg <- solar %>%
   group_by(operating_quarter, regulated) %>% 
   summarise(tot_cap = sum(nameplate_cap)) 
-
 
 ggplot(solar_reg, aes(x=operating_quarter,y=tot_cap, fill=regulated)) +
   geom_bar(stat='identity',position='stack') +
@@ -218,45 +182,14 @@ ggsave(paste0(figDir,"solar_reg.png"),width=8,height=11)
 
 # look geographically, southern spots not as clumped in back half of year
 
-
-solar_region <- solar_raw %>%
-  filter(!(sector_name %in% omitList),
-         operating_year >= 2010,
-         region != "Hawaii") %>%
-  group_by(operating_quarter, region) %>% 
-  summarise(d_cap = sum(nameplate_cap)) %>% 
-  arrange(operating_quarter,region)  
-  
-ggplot(solar_region, aes(x=operating_quarter,y=d_cap, fill=region)) +
-  geom_bar(stat='identity',position='stack') +
-  scale_x_yearqtr(format = "%YQ%q", 
-                  breaks = seq(from = min(solar$operating_quarter)+.75, 
-                               to = max(solar$operating_quarter), by = 1),
-                  limits=c(min(solar$operating_quarter), 
-                           max=max(solar$operating_quarter))) +
-  theme_bw() + 
-  facet_wrap(~region, nrow = 2, scales="free") + 
-  xlab("\nStart Operation Date") +
-  ylab("Capacity Additions (MW)\n") + 
-  geom_vline(xintercept=as.yearqtr("2016 Q4"), linetype='dotted') +
-  theme(axis.text.x = element_text(angle=90),
-        legend.position="none")
-
-ggsave(paste0(figDir, "solar_regional.png"),width=12,height=7)
-
-
-solar_cumsum_region <- solar_raw %>% filter(!(sector_name %in% omitList),
-                                            operating_year >= 2010,
-                                            region != "Hawaii") %>%
-  group_by(operating_quarter,region) %>% 
+solar %>% group_by(operating_quarter,region) %>% 
   summarise(d_cap = sum(nameplate_cap)) %>% 
   ungroup() %>%
   group_by(region) %>% 
   mutate(tot_cap = cumsum(d_cap),
          d_cap_p = d_cap/tot_cap) %>% 
-  arrange(region, operating_quarter) 
-
-ggplot(solar_cumsum_region, aes(x=operating_quarter,y=tot_cap, fill=region)) +
+  arrange(region, operating_quarter) %>%
+ggplot(aes(x=operating_quarter,y=tot_cap, fill=region)) +
   geom_bar(stat='identity',position='stack') +
   scale_x_yearqtr(format = "%YQ%q", 
                   breaks = seq(from = min(solar$operating_quarter)+.75, 
@@ -271,7 +204,7 @@ ggplot(solar_cumsum_region, aes(x=operating_quarter,y=tot_cap, fill=region)) +
   theme(axis.text.x = element_text(angle=90),
         legend.position="none")
 
-ggsave(paste0(figDir,"dead_idea.png"),width=12, height=7)
+ggsave(paste0(figDir,"tot_cap_region.png"),width=12, height=7)
 
 
 
@@ -292,3 +225,30 @@ solar_raw %>% filter(state=="MA", operating_year < 2015) %>%
          utility_name) %>%
   View()
  
+
+
+# Work in the utility data ------------------------------------------------
+
+# is there a region variable? 
+gen_data_merged %>% group_by(street_address, technology) %>% 
+  summarise(tot_cap = sum(nameplate_cap),
+            nUnits = n()) %>% 
+  filter(technology == "Solar Photovoltaic") %>% arrange(desc(tot_cap)) %>%
+  ungroup() %>% 
+  mutate(share = tot_cap/sum(tot_cap)) %>% View()
+
+gen_data_merged %>% filter(technology == "Solar Photovoltaic",
+                           operating_year > 2008) %>%
+  group_by(operating_quarter, street_address) %>% 
+  summarise(tot_cap = sum(nameplate_cap),
+            nUnits = n()) %>% 
+  ungroup() %>% group_by(operating_quarter) %>%
+  mutate(share = tot_cap/sum(tot_cap)) %>%  
+  arrange(operating_quarter, desc(tot_cap)) %>% 
+  filter(share >= 0.05) %>% 
+  ggplot(aes(x=operating_quarter,y=tot_cap, fill=street_address)) +
+  geom_bar(stat='identity',position='stack') +
+  theme(legend.position = "none")
+
+
+# Make data for regressions  -----------------------------------------------------
